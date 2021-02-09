393afa13edd7d5a3de20916f599615f88d4b73ef
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-02-09 11:06:43 +0000

    DRTII-725 adding defail cors

--- src/main/scala/uk/gov/homeoffice/drt/IEIDashboardServer.scala
+++ src/main/scala/uk/gov/homeoffice/drt/IEIDashboardServer.scala
@@ -3,19 +3,32 @@ package uk.gov.homeoffice.drt
 import cats.effect.{ConcurrentEffect, ContextShift, Timer}
 import cats.implicits._
 import fs2.Stream
+import org.http4s.HttpRoutes
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware.{AutoSlash, CORS, Logger, Timeout}
 import uk.gov.homeoffice.drt.api.{ArrivalRoutes, PublicRoutes}
 import uk.gov.homeoffice.drt.applicative.ArrivalFlights
 import uk.gov.homeoffice.drt.repository.{ArrivalRepository, DepartureRepository}
 import uk.gov.homeoffice.drt.service.{AirlineService, ArrivalService, CiriumService}
 
 import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
 
 object IEIDashboardServer {
   def stream[F[_] : ConcurrentEffect](cfg: Config)(implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
+
+    val middleware: HttpRoutes[F] => HttpRoutes[F] = {
+      { http: HttpRoutes[F] =>
+        AutoSlash(http)
+      } andThen { http: HttpRoutes[F] =>
+        CORS(http, CORS.DefaultCORSConfig)
+      } andThen { http: HttpRoutes[F] =>
+        Timeout(60.seconds)(http)
+      }
+    }
+
     for {
       _ <- BlazeClientBuilder[F](global).stream
 
@@ -33,7 +46,7 @@ object IEIDashboardServer {
 
       arrivalFlightsAlg = ArrivalFlights.impl[F](arrivalsService, ciriumService)
 
-      httpApp = (
+      httpApp = middleware(
         PublicRoutes.dashboardRoutes[F]() <+>
           PublicRoutes.airlineRoutes[F](cfg.airline, airlineService) <+>
           ArrivalRoutes.arrivalFlightsRoutes[F](arrivalFlightsAlg, cfg.api.permissions)
