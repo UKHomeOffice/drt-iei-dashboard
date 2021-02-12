eab0d6badbd80e6c89cc34d6ea3c8628904152e6
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-02-12 15:12:51 +0000

    DRTII-725 remove comments

--- src/main/scala/uk/gov/homeoffice/drt/utils/DateUtil.scala
+++ src/main/scala/uk/gov/homeoffice/drt/utils/DateUtil.scala
@@ -19,8 +19,6 @@ object DateUtil {
     date1.toInstant().atZone(ZoneOffset.UTC).toLocalDate.
       compareTo(date2.toInstant().atZone(ZoneOffset.UTC).toLocalDate) == 0
 
-//  val `yyyy-MM-ddTHH:mm:ss.SSSZ` : String => Date = date => new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(date)
-
   val `yyyy-MM-ddTHH:mm:ss.SSSZ_parse_toLocalDateTime`: String => LocalDateTime = dateString => LocalDateTime.parse(dateString, `yyyy-MM-ddTHH:mm:ss.SSSZ_LocalDateFormatter`)
 
   val `yyyy-MM-ddTHH:mm:ss.SSSZ_LocalDateFormatter` = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
--- src/test/scala/uk/gov/homeoffice/drt/BaseSpec.scala
+++ src/test/scala/uk/gov/homeoffice/drt/BaseSpec.scala
@@ -6,6 +6,4 @@ import scala.concurrent.ExecutionContext
 
 trait BaseSpec {
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-  //  implicit val timer: Timer[IO]     = IO.timer(ExecutionContext.global)
-  //  implicit val concurrentEffect: ConcurrentEffect[IO] = IO.ioConcurrentEffect
 }
