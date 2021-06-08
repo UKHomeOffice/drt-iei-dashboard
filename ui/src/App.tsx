35c2c81315d3620cf02e382713fb877278cf862b
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-06-08 12:06:01 +0100

    DRTII-823 formatting typescript files

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import './App.css';
 import DatePicker from './components/DatePicker'
 import ErrorBoundary from './components/ErrorBoundary'
- import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Grid container spacing={3}>
-                <Grid item xs={12} sm={4}>
-                   <div className="image-div"><img src="favicon.png" alt="" className="image-homeOffice"/> <span className="image-text">Home Office</span></div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={4}>
+                        <div className="image-div"><img src="favicon.png" alt="" className="image-homeOffice"/> <span
+                            className="image-text">Home Office</span></div>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <h2>DRT IEI Dashboard</h2>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    <h2>DRT IEI Dashboard</h2>
-                </Grid>
-            </Grid>
-            <ErrorBoundary>
-               <DatePicker/>
-           </ErrorBoundary>
-      </header>
-    </div>
-  );
+                <ErrorBoundary>
+                    <DatePicker/>
+                </ErrorBoundary>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- ui/src/components/DatePicker.tsx
+++ ui/src/components/DatePicker.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
 } from '@material-ui/pickers';
 import SearchFilters from './SearchFilters';
 import Grid from '@material-ui/core/Grid';
@@ -12,49 +12,49 @@ interface IProps {
 }
 
 interface IState {
-  selectedDate?: Date
+    selectedDate?: Date
 }
 
 export default class MaterialUIPickers extends React.Component<IProps, IState> {
 
-  handleDateChange = (sDate: Date | null) => {
-    let selectedDate = sDate as Date
-    this.setState({...this.state, selectedDate: selectedDate});
-  };
-
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      selectedDate: new Date()
+    handleDateChange = (sDate: Date | null) => {
+        let selectedDate = sDate as Date
+        this.setState({...this.state, selectedDate: selectedDate});
     };
-  }
 
-  componentDidMount() {
-    console.log("this.state.selectedDate" + this.state.selectedDate)
-  }
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            selectedDate: new Date()
+        };
+    }
 
-  render() {
-    return (
-      <div style={{width: 1100}}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="date-picker-dialog"
-                  label="Flight departure date"
-                  format="yyyy-MM-dd"
-                  value={this.state.selectedDate}
-                  onChange={this.handleDateChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
-                />
-              </MuiPickersUtilsProvider>
-          </Grid>
-        </Grid>
-        <SearchFilters pickedDate={format(this.state.selectedDate as Date, "yyyy-MM-dd")}/>
-      </div>
-    );
-  }
+    componentDidMount() {
+        console.log("this.state.selectedDate" + this.state.selectedDate)
+    }
+
+    render() {
+        return (
+            <div style={{width: 1100}}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <KeyboardDatePicker
+                                margin="normal"
+                                id="date-picker-dialog"
+                                label="Flight departure date"
+                                format="yyyy-MM-dd"
+                                value={this.state.selectedDate}
+                                onChange={this.handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                            />
+                        </MuiPickersUtilsProvider>
+                    </Grid>
+                </Grid>
+                <SearchFilters pickedDate={format(this.state.selectedDate as Date, "yyyy-MM-dd")}/>
+            </div>
+        );
+    }
 }
