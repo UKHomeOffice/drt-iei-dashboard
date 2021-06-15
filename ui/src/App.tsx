1cc6f1a2c03ea7c25d7b5674439f92bbc2b0205c
Nilesh Gupta <guptanilesh13@gmail.com>
2021-06-15 15:58:04 +0100

    Drtii 823 style (#11)
    * DRTII-823 update styling change
    
    * DRTII-823 revert back grid for headers
    
    Co-authored-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,22 +4,25 @@ import DatePicker from './components/DatePicker'
 import ErrorBoundary from './components/ErrorBoundary'
 import Grid from '@material-ui/core/Grid';
 
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={4}>
-                        <div className="image-div"><img src="favicon.png" alt="" className="image-homeOffice"/> <span
-                            className="image-text">Home Office</span></div>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <h2>DRT IEI Dashboard</h2>
-                    </Grid>
+            <Grid container>
+                <Grid item xs={12} sm={4}>
+                    <div className="image-div">
+                        <img src="favicon.png" alt="" className="image-homeOffice"/>
+                        <span className="image-text">Home Office</span>
+                    </div>
                 </Grid>
-                <ErrorBoundary>
-                    <DatePicker/>
-                </ErrorBoundary>
+                <Grid item xs={12} sm={4}>
+                   <h3>DRT IEI Dashboard</h3>
+                </Grid>
+            </Grid>
+            <ErrorBoundary>
+                <DatePicker/>
+            </ErrorBoundary>
             </header>
         </div>
     );
