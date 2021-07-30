072ed2c142b5be35e35b27be7f4502cdd0ef9379
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-07-30 14:29:21 +0100

    DRTII-867 applying port filter

--- src/main/scala/uk/gov/homeoffice/drt/model/FlightsRequest.scala
+++ src/main/scala/uk/gov/homeoffice/drt/model/FlightsRequest.scala
@@ -1,4 +1,4 @@
 package uk.gov.homeoffice.drt.model
 
-final case class FlightsRequest(region: String, post: String, country: String, date: String, timezone: String)
+final case class FlightsRequest(region: String, post: String, country: String, portList: List[String], date: String, timezone: String)
 
